38b97f4f2d96efbd559ab0eb9ed1b13c27ab801a
praneelpp <142261163+praneelpp@users.noreply.github.com>
2024-02-20 23:25:45 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,30 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-require('dotenv').config()
-
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const { ethers } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
+  const privateKey = process.env.PRIVATE_KEY;
+  const networkAddress = "https://ethereum-goerli.publicnode.com";
 
-    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-  
-    const tx = await token.mint(walletAddress, 1000);
-    await tx.wait();
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+  const signer = new ethers.Wallet(privateKey, provider);
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+  const contractAddress = "0x3951cB7D5Ad052D1c773f4d1e7320A10EACfbC02";
+
+  const Cat_contract = await ethers.getContractFactory("Cat", signer);
+  const cat = await Cat_contract.attach(contractAddress);
+
+  await cat.mint(5);
+  console.log("Number of NFTs minted:5");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
